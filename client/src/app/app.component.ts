cde2a5bc91e5bbde133ad6243be0422e833b9118
Buhle Pikoli <s221524339@mandela.ac.za>
2023-07-28 12:54:54 +0200

    End of Section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product.model';
+import { Pagination } from './models/pagination.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'BuSiphe';
+
+  products: Product[] = [];
+
+  constructor(private http: HttpClient){
+  }
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: response => this.products = response.data,
+      error: err => console.error(err),
+      complete: () =>{
+        console.log("request completed");
+        console.log("extra statement");
+      }
+    })
+  }
 }
