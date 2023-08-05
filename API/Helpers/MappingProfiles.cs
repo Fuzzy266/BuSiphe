e3a58d3e07bdb5d0044df05cd5ea5be51caf3bf8
Buhle Pikoli <s221524339@mandela.ac.za>
2023-08-05 11:27:08 +0200

    End of Section 18

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -2,6 +2,7 @@ using API.DTOs;
 using AutoMapper;
 using Core.Entities;
 using Core.Entities.Identity;
+using Core.Entities.OrderAggregate;
 
 namespace API.Helpers
 {
@@ -10,13 +11,22 @@ namespace API.Helpers
         public MappingProfiles()
         {
             CreateMap<Product, ProductDTO>()
-            .ForMember(d => d.ProductBand, opt => opt.MapFrom(s => s.ProductBand.Name))
-            .ForMember(d => d.ProductType, opt => opt.MapFrom(s => s.ProductType.Name))
-            .ForMember(d => d.PictureUrl, opt => opt.MapFrom<ProductUrlResolver>());
+                .ForMember(d => d.ProductBand, opt => opt.MapFrom(s => s.ProductBand.Name))
+                .ForMember(d => d.ProductType, opt => opt.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.PictureUrl, opt => opt.MapFrom<ProductUrlResolver>());
 
-            CreateMap<Address, AddressDto>().ReverseMap();
+            CreateMap<Core.Entities.Identity.Address, AddressDto>().ReverseMap();
             CreateMap<CustomerBasketDto, CustomerBasket>();
             CreateMap<BasketItemDto, BasketItem>();
+            CreateMap<AddressDto, Core.Entities.OrderAggregate.Address>();
+            CreateMap<Order, OrderToReturnDto>()
+                .ForMember(d => d.DeliveryMethod, o => o.MapFrom(s => s.DeliveryMethod.ShortName))
+                .ForMember(d => d.ShippingPrice, o => o.MapFrom(s => s.DeliveryMethod.Price));
+            CreateMap<OrderItem, OrderItemDto>()
+                .ForMember(d => d.ProductId, o => o.MapFrom(s => s.ItemOrdered.ProductItemId))
+                .ForMember(d => d.ProductName, o => o.MapFrom(s => s.ItemOrdered.ProductName))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom(s => s.ItemOrdered.PictureUrl))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<OrderItemUrlResolver>());
         }
 
     }
